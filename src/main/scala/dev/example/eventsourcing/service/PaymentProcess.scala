4d862e07022362e3dd09b218a8cbca87c0817574
Martin Krasser <krasserm@googlemail.com>
2011-12-29 10:08:12 +0100

    Some hints how to implement failure-recovery for payment process

--- src/main/scala/dev/example/eventsourcing/service/PaymentProcess.scala
+++ src/main/scala/dev/example/eventsourcing/service/PaymentProcess.scala
@@ -4,6 +4,13 @@ import dev.example.eventsourcing.domain._
 import dev.example.eventsourcing.event._
 
 trait PaymentProcess extends ChannelSubscriber[EventLogEntry] {
+  // TODO: failure recovery for payment process (e.g. after a node crash)
+  // 
+  // - store id of last processed event (e.g. using EventProjection and a storage mechanism)
+  // - execute process state changes and paymentService/invoiceService updates within same tx
+  // - replay events for payment process on appserver boot (beginning after last stored event)
+  // - requires a transactional paymentService (invoiceService is transactional)
+
   def paymentService: PaymentService
   def invoiceService: InvoiceService
 
